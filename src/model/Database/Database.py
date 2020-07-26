2a80f1a2475755f128741f5984e8062b7061bb66
KennethS0 <kenneth.sanchez0609@gmail.com>
2020-07-25 22:07:04 -0600

    Constants

--- src/model/Database/Database.py
+++ src/model/Database/Database.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import model.Database.IConstants
+import model.Database.IConstants as I
 import model.User.User as u
 
 '''
@@ -61,7 +61,7 @@ class Database:
         try:
             cursor = self.connection.cursor()
 
-            cursor.callproc('SignUp_Person', (kwargs['citizenId'],
+            cursor.callproc(I.SIGN_UP_PERSON, (kwargs['citizenId'],
                                              kwargs['firstName'].upper(), 
                                              kwargs['secondName'].upper(),
                                              kwargs['lastName'].upper(),
@@ -72,7 +72,7 @@ class Database:
                                              kwargs['nationalityId'], 
                                              kwargs['communityId']))
 
-            cursor.callproc('SignUp_Account', (kwargs['citizenId'], 
+            cursor.callproc(I.SIGN_UP_PERSON, (kwargs['citizenId'], 
                                            kwargs['username'].upper(),
                                            kwargs['password']))
 
@@ -91,7 +91,7 @@ class Database:
         try:
             cursor = self.connection.cursor()
 
-            cursor.callproc('LogIn_User', (pUsername, pPassword))
+            cursor.callproc(I.LOGIN, (pUsername, pPassword))
 
             userData = []
 
@@ -103,7 +103,7 @@ class Database:
             if userData:
                 self.connectedUser = u.User(userData[0][1], userData[0][0])
                 
-                cursor.callproc('LogIn_IsAdmin', (userData[0][0],))
+                cursor.callproc(I.IS_ADMIN, (userData[0][0],))
                 
                 # Sets the admin state in the user
                 for isAdmin in cursor.stored_results():
@@ -117,25 +117,3 @@ class Database:
 
         finally:
             cursor.close()
-
-
-
-    def __getPublicKey(self):
-        '''
-        Method to get the public key in order to encrypt messages. 
-        Method turned private for security.
-        '''
-        return rsa.PublicKey(7448188390935309559762268346969139335445643747358375780699545774849146587133817358590827902682907530426006839207101084773877149373355688748172562976675587, 
-                             65537)
-    
-    
-    def __getPrivateKey(self):
-        '''
-        Method to get the private key in order to decrypt messages.
-        Method turned into private key for security.
-        '''
-        return rsa.PrivateKey(7448188390935309559762268346969139335445643747358375780699545774849146587133817358590827902682907530426006839207101084773877149373355688748172562976675587, 
-                              65537, 
-                              58415381127313565065807191820530961417505544747886005634673032459106479444200234828576816576660741873314729542542260093985551896547099833610253148818145, 
-                              4770878076950975455204481644330156637049818655614541034913282465696905611356178713, 
-                              1561177684862443317707859093150293908187485696864728316834700491281791099)
---
+++ src/model/Database/IConstants.py
@@ -0,0 +1,12 @@
+##################
+# SIGN UP MODULE #
+##################
+SIGN_UP_PERSON = 'SignUp_Person'
+SIGN_UP_ACCOUNT = 'SignUp_Account'
+
+
+#################
+# LOG IN MODULE #
+#################
+LOGIN = 'LogIn_User'
+IS_ADMIN = 'LogIn_IsAdmin'
