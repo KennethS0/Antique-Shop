e1cd84d0ef0b6147d7ce4ab384b7a6e03160184d
Calo <carlos.roberto.kruse@gmail.com>
2020-07-25 23:36:14 -0600

    Finished Admin Queries

--- SQL Scripts/Queries/Admin Queries/6 - Min Max Prices.sql
+++ SQL Scripts/Queries/Admin Queries/6 - Min Max Prices.sql
@@ -1,6 +1,6 @@
 DELIMITER ;;
 DROP PROCEDURE IF EXISTS min_max_prices;;
-CREATE PROCEDURE min_max_prices(IN top INT)
+CREATE PROCEDURE min_max_prices()
 	BEGIN
 		SELECT ge.productcategory.name AS category, MAX(ge.product.price) AS maximum, MIN(ge.product.price) AS minimum
 		FROM ge.product 
