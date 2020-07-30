df2a94130cae7067833e7dbd0b61abe1d8c2e07a
Calo <carlos.roberto.kruse@gmail.com>
2020-07-29 19:49:18 -0600

    Minor changes

--- src/Controller/ProductviewController.py
+++ src/Controller/ProductviewController.py
@@ -17,16 +17,21 @@ class ProductviewController:
             self.product = self.model.query(I.GET_PRODUCT, [productid])
             print(self.product)
             #print("ProductviewController: PRODUCT SET: " + str(productid))
+
+            #self.view.ui.Product_NameLabel.setText("")
+            
             self.view.ui.Product_NameDisplay.setText(str(self.product[0][1]))
             self.view.ui.Product_ViaDisplay.setText(str(self.product[0][2]))
             self.view.ui.Product_SellerDisplay.setText(str(self.product[0][3]))
-            self.view.ui.Product_PriceDisplay.setText(str(self.product[0][4]))
+            self.view.ui.Product_PriceDisplay.setText(str(self.product[0][4]) + " $")
 
             self.image = QtGui.QPixmap(str(self.product[0][5]))
             self.view.ui.Product_PhotoDisplay.setPixmap(self.image)
 
             self.view.ui.Product_DescriptionDisplay.setWordWrap(True)
             self.view.ui.Product_DescriptionDisplay.setText(str(self.product[0][6]))
+
+            
 
         except Exception as err:
 
